0bb5b46be85a05e04b1cad7d48b25491418cb840
valentin coulomb <valentin.coulomb2@gmail.com>
2021-10-13 16:14:50 +0200

    make it better

---
+++ src/Entity/Manga.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MangaRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+/**
+ * @ORM\Entity(repositoryClass=MangaRepository::class)
+ * @ApiResource
+ * @ApiFilter(SearchFilter::class)
+ */
+class Manga
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isRead;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $isStart;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imgUrl;
+
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isOver;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateEnd;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIsRead(): ?bool
+    {
+        return $this->isRead;
+    }
+
+    public function setIsRead(bool $isRead): self
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    public function getIsStart(): ?string
+    {
+        return $this->isStart;
+    }
+
+    public function setIsStart(string $isStart): self
+    {
+        $this->isStart = $isStart;
+
+        return $this;
+    }
+
+    public function getImgUrl(): ?string
+    {
+        return $this->imgUrl;
+    }
+
+    public function setImgUrl(string $imgUrl): self
+    {
+        $this->imgUrl = $imgUrl;
+
+        return $this;
+    }
+
+
+    public function getIsOver(): ?bool
+    {
+        return $this->isOver;
+    }
+
+    public function setIsOver(bool $isOver): self
+    {
+        $this->isOver = $isOver;
+
+        return $this;
+    }
+
+    public function getDateEnd(): ?\DateTimeInterface
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(?\DateTimeInterface $dateEnd): self
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+}
---
+++ src/Repository/MangaRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Manga;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Manga|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Manga|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Manga[]    findAll()
+ * @method Manga[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MangaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Manga::class);
+    }
+
+    // /**
+    //  * @return Manga[] Returns an array of Manga objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('m.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Manga
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
